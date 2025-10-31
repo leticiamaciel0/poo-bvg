caf73a51ec373fe2b1bb8b679c4503c9a62622a1
leticiamaciel0 <leticia.maciel09@aluno.ifce.edu.br>
2025-10-31 09:27:51 -0300

    Criação da estrutura inicial do projeto com main.py e pasta src
    Criação da estrutura base do projeto, incluindo o arquivo principal main.py e a pasta src para armazenar o módulo da classe Cliente.

---
+++ Projeto_1/main.py
@@ -0,0 +1,13 @@
+from src.clientes import Cliente 
+
+cliente1 = Cliente("leticia",22,500)
+
+cliente1.mostrar_informacoes()
+cliente1.atualizar_saldo(500)
+cliente1.mostrar_informacoes()
+
+cliente2 = Cliente("david",20,700)
+
+cliente2.mostrar_informacoes()
+cliente2.atualizar_saldo(500)
+cliente2.mostrar_informacoes()
---
+++ Projeto_1/src/clientes.py
@@ -0,0 +1,12 @@
+class Cliente:
+    def __init__(self,nome,idade,saldo):
+        self.nome=nome
+        self.idade=idade
+        self.saldo=saldo
+
+    def mostrar_informacoes(self):
+        print(f"Cliente:{self.nome}, idade:{self.idade}, saldo: {self.saldo}")
+
+    def atualizar_saldo(self,valor):
+        self.saldo = self.saldo + valor
+   
